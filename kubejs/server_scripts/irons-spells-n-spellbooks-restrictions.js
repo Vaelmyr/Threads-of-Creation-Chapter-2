e6e1a7b7c615e643df857f1a727e9a098f3391c9
Vaelmyr <sdistefano.dev@gmail.com>
2026-03-15 11:07:53 +0100

    Fixed "Iron's Spellbook Restriction" missing crafting

--- kubejs/server_scripts/irons-spells-n-spellbooks-restrictions.js
+++ kubejs/server_scripts/irons-spells-n-spellbooks-restrictions.js
@@ -12,3 +12,59 @@ LootJS.modifiers((event) => {
                 .randomChance(0.3)
         );
 });
+
+ServerEvents.recipes((event) => {
+    ['common', 'uncommon', 'rare', 'epic', 'legendary'].forEach((rarity) => {
+        event.custom({
+            type: 'irons_spellbooks:alchemist_cauldron_brew',
+            base_fluid: {
+                id: `irons_spellbooks:${rarity}_ink`,
+                amount: 1000
+            },
+            input: {
+                item: 'irons_spellbooks:upgrade_orb'
+            },
+            results: [],
+            byproduct: {
+                id: `irons_restrictions:${rarity}_upgrade`
+            }
+        })
+            .id(`irons_restrictions:alchemist_cauldron/soak_${rarity}_upgrade`);
+
+        event.custom({
+            type: 'irons_spellbooks:alchemist_cauldron_fill',
+            input: {
+                item: `irons_restrictions:${rarity}_upgrade`
+            },
+            result: {
+                id: 'irons_spellbooks:upgrade_orb',
+                count: 1
+            },
+            fluid: {
+                id: `irons_spellbooks:${rarity}_ink`,
+                amount: 1000
+            },
+            mustFitAll: true
+        })
+            .id(`irons_restrictions:alchemist_cauldron/fill_${rarity}_upgrade`);
+    });
+
+    event.shapeless(
+        Item.of('irons_restrictions:fragment', 2),
+        ['irons_spellbooks:scroll']
+    )
+        .id('irons_restrictions:fragment');
+
+
+    event.shapeless(
+        'irons_restrictions:unfinished_manuscript',
+        [
+            'irons_restrictions:fragment',
+            'irons_spellbooks:magic_cloth',
+            'irons_spellbooks:magic_cloth',
+            'irons_spellbooks:magic_cloth',
+            'irons_spellbooks:magic_cloth'
+        ]
+    )
+        .id('ironsrestrictions:unfinished_manuscript');
+});
